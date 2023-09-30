2c4d29dcc8c48986b994417eb84be4595a43f4f9
omeraran <omeraranofficial@gmail.com>
2023-09-30 22:46:05 +0300

    added getshipmentSize method to determine shipment size of a basket and compare method in ShipmentSize class.

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,20 +1,53 @@
 package com.trendyol.shipment;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Basket {
 
+    private final static Integer THRESHOLD_VALUE = 3;
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        if (hasThreeOrMoreProducts()) {
+            for (ShipmentSize size : ShipmentSize.values()) {
+                if (hasThreeOrMoreSameShipmentSize(size)) {
+                    return getOneBiggerSize(size);
+                }
+            }
+        }
+        return getBiggestShipmentSize();
     }
 
-    public List<Product> getProducts() {
-        return products;
+    private Boolean hasThreeOrMoreSameShipmentSize(ShipmentSize shipmentSize) {
+        List<ShipmentSize> shipmentSizes = products.stream().map(Product::getSize).collect(Collectors.toList());
+        return shipmentSizes.stream().filter(x -> x == shipmentSize).count() >= THRESHOLD_VALUE;
+    }
+
+    private Boolean hasThreeOrMoreProducts() {
+        return products.size() >= THRESHOLD_VALUE;
+    }
+
+    private ShipmentSize getBiggestShipmentSize() {
+        ShipmentSize biggestShipmentSize = ShipmentSize.SMALL;
+        for (Product product : products) {
+            biggestShipmentSize = product.getSize().compare(biggestShipmentSize);
+        }
+        return biggestShipmentSize;
     }
 
     public void setProducts(List<Product> products) {
         this.products = products;
+    }
+
+    private ShipmentSize getOneBiggerSize(ShipmentSize size) {
+        switch (size) {
+            case SMALL:
+                return ShipmentSize.MEDIUM;
+            case MEDIUM:
+                return ShipmentSize.LARGE;
+            default:
+                return ShipmentSize.X_LARGE;
+        }
     }
 }
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -5,5 +5,14 @@ public enum ShipmentSize {
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+    
+    public ShipmentSize compare(ShipmentSize size) {
+        if (this.ordinal() >= size.ordinal()) {
+            return this;
+        }
+        else {
+            return size;
+        }
+    }
 }
